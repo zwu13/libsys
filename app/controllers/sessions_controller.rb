f6bf606af26a3b102fa0043e5ceb1798ef208f05
zwu13 <zwu13@nvsu.edu>
2015-09-23 03:30:05 +0000

    Finish admin login and logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    admin = Admin.find_by(email: params[:session][:email].downcase)
+    if admin && admin.authenticate(params[:session][:password])
+      log_in admin
+      redirect_to admin
+    else
+     render 'new'
+    end
+  end
+  
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
