afa93a1cd2841f58e975c69783e936fbba36fa09
zwu13 <zwu13@nvsu.edu>
2015-09-24 02:32:19 +0000

    Finish delete and preconfigured admin

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,4 +1,6 @@
 class AdminsController < ApplicationController
+  before_action :logged_in_admin, only: [:show, :edit, :update, :destroy]
+  before_action :correct_admin, only: [:show, :edit, :update]
   def show
     @admin = Admin.find(params[:id])
   end
@@ -28,10 +30,32 @@ class AdminsController < ApplicationController
       render 'edit'
     end
   end
+  
+  def index
+    @admins = Admin.all
+  end
+
+  def destroy
+    if ( params[:id].to_i != 1 ) && (Admin.find(params[:id]) != current_admin)
+      Admin.find(params[:id]).destroy
+    end
+    redirect_to admins_url
+  end
 
   private
   
     def admin_params
       params.require(:admin).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+    def logged_in_admin
+      unless logged_in?
+        redirect_to login_url
+      end
+    end
+    
+    def correct_admin
+      @admin = Admin.find(params[:id])
+      redirect_to(root_url) unless @admin == current_admin
     end
 end
