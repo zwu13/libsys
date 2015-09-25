b12ec657b293ca67abb0050ce534bf4f0a42fb6a
zwu13 <zwu13@nvsu.edu>
2015-09-25 03:17:24 +0000

    Finish member signup

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,4 +1,22 @@
 class MembersController < ApplicationController
+  def show
+    @member = Member.find(params[:id])
+  end
+
   def new
+    @member = Member.new
+  end
+  
+  def create
+    @member = Member.new(admin_params)
+    if @member.save
+      redirect_to @member
+    else
+      render 'new'
+    end
+  end
+  
+  def member_params
+      params.require(:member).permit(:name, :email, :password, :password_confirmation)
   end
 end
